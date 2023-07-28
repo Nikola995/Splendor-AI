1114c8181f26df88d0172374aa5ef5714679af6f
Nikola Spasovski <nikola221295@hotmail.com>
2023-07-28 11:51:24 +0200

    Refactored Noble and added NobleGenerator

--- game_base/nobles.py
+++ game_base/nobles.py
@@ -1,34 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May  2 17:05:45 2022
+from dataclasses import dataclass
+from typing import List
+from random import shuffle
+from .tokens import Token, TokenBag
 
-@author: Nikola
-"""
-from dataclasses import dataclass, field
 
-@dataclass(frozen = True)
+@dataclass(frozen=True, slots=True)
 class Noble:
-    #Number of bonuses per color (type) required to acquire noble
-    bonus_required: dict[str,int] = field(default_factory=lambda: ({"green":0,
-                                           "white":0,
-                                           "blue":0,
-                                           "black":0,
-                                           "red":0}))
-    
-    #Number of prestige points the noble is worth
-    #default = 3
+    # Number of bonuses per color required to acquire noble
+    bonus_required: TokenBag
+    # Number of prestige points the noble is worth
     prestige_points: int = 3
-    
-def generate_nobles():
-    nobles = []
-    nobles.append(Noble({"green":4,"white":0,"blue":0,"black":0,"red":4}))
-    nobles.append(Noble({"green":0,"white":3,"blue":0,"black":3,"red":3}))
-    nobles.append(Noble({"green":0,"white":4,"blue":4,"black":0,"red":0}))
-    nobles.append(Noble({"green":0,"white":4,"blue":0,"black":4,"red":0}))
-    nobles.append(Noble({"green":4,"white":0,"blue":4,"black":0,"red":0}))
-    nobles.append(Noble({"green":3,"white":0,"blue":3,"black":0,"red":3}))
-    nobles.append(Noble({"green":3,"white":3,"blue":3,"black":0,"red":0}))
-    nobles.append(Noble({"green":0,"white":0,"blue":0,"black":4,"red":4}))
-    nobles.append(Noble({"green":0,"white":3,"blue":3,"black":3,"red":0}))
-    nobles.append(Noble({"green":3,"white":0,"blue":0,"black":3,"red":3}))
-    return nobles
+
+
+@dataclass
+class NobleGenerator:
+    """Generates a List of Nobles for the number of players in a game."""
+
+    def default_nobles_list(self) -> List[Noble]:
+        """Returns a list of all the default nobles."""
+        return [Noble({Token.GREEN: 4, Token.WHITE: 0, Token.BLUE: 0,
+                       Token.BLACK: 0, Token.RED: 4}),
+                Noble({Token.GREEN: 0, Token.WHITE: 3, Token.BLUE: 0,
+                       Token.BLACK: 3, Token.RED: 3}),
+                Noble({Token.GREEN: 0, Token.WHITE: 4, Token.BLUE: 4,
+                       Token.BLACK: 0, Token.RED: 0}),
+                Noble({Token.GREEN: 0, Token.WHITE: 4, Token.BLUE: 0,
+                       Token.BLACK: 4, Token.RED: 0}),
+                Noble({Token.GREEN: 4, Token.WHITE: 0, Token.BLUE: 4,
+                       Token.BLACK: 0, Token.RED: 0}),
+                Noble({Token.GREEN: 3, Token.WHITE: 0, Token.BLUE: 3,
+                       Token.BLACK: 0, Token.RED: 3}),
+                Noble({Token.GREEN: 3, Token.WHITE: 3, Token.BLUE: 3,
+                       Token.BLACK: 0, Token.RED: 0}),
+                Noble({Token.GREEN: 0, Token.WHITE: 0, Token.BLUE: 0,
+                       Token.BLACK: 4, Token.RED: 4}),
+                Noble({Token.GREEN: 0, Token.WHITE: 3, Token.BLUE: 3,
+                       Token.BLACK: 3, Token.RED: 0}),
+                Noble({Token.GREEN: 3, Token.WHITE: 0, Token.BLUE: 0,
+                       Token.BLACK: 3, Token.RED: 3})]
+
+    def generate_nobles(self, num_players: int = 4) -> List[Noble]:
+        """Returns n + 1 nobles for n players from a shuffled list."""
+        return shuffle(self.default_nobles_list())[0:num_players + 1]
