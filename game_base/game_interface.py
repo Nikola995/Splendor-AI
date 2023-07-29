adac2894a44ee2fefad5da6f79f8269e32783fa0
Nikola Spasovski <nikola221295@hotmail.com>
2023-07-29 14:55:49 +0200

    Added print for data in game but cards on table

--- game_base/game_interface.py
+++ game_base/game_interface.py
@@ -45,61 +45,47 @@ class GameInterfaceConsole(GameInterface):
                 self.player_turn()
 
     def show_game_meta_data(self) -> None:
-        print("Game meta-data"
-              f"\nGame is {self.game.meta_data.state.name}"
-              f"\nTurns played: {self.game.meta_data.turns_played}")
-        if self.game.meta_data.state == GameState.IN_PROGRESS:
-            print("Current player to move: "
-                  f"{self.game.current_player().player_id}")
-        
+        print("----------Game meta-data---------------")
+        print(str(self.game.meta_data))
+
+    def show_game_nobles(self) -> None:
+        print("----------Available Nobles-------------")
+        for noble in self.game.nobles:
+            print("_______________________________________")
+            print(str(noble))
+
+    def show_game_bank(self) -> None:
+        print("----------Bank-------------------------")
+        print(str(self.game.bank))
+
+    def show_game_players(self) -> None:
+        print("----------Players----------------------")
+        for player in self.game.players:
+            print("_______________________________________")
+            print(str(player))
+
     def show_game_state(self, dense=0) -> None:
-        """Return the state of the game."""
-        # TODO implement a state representation
-        # TODO add a game_id asset
-        # This means also get state for players, bank, cards on table
+        """Prints the state of the game on the console."""
         # dense - if true, return values as int (ex. player blue tokens = 5)
         # else return as sparse matrix (
         # (ex. player blue tokens = [1 1 1 1 1 0 0]))
-        # verbose - print out the state in pretty string
-        print('Current game state')
+        # print("----------G----------------------------")
+        print("----------Complete game state----------")
+        print("_______________________________________")
         self.show_game_meta_data()
         if self.game.meta_data.state == GameState.NOT_STARTED:
             return None
-        # Print bank
-        print('\nBank tokens available:')
-        print('Color\tAmount')
-        for color in self.bank.token_available:
-            print(f'{color:<5}\t{self.bank.token_available[color]}')
-        # Print players
-        print('\nPlayers stats')
-        for player in self.players:
-            print(f'{player.player_id}')
-            print(f'Prestige points: {player.prestige_points}')
-            print('Color\tReserved tokens\tOwned bonuses')
-            for color in player.bonus_owned:
-                print(f'{color:<5}\t'
-                        f'{player.token_reserved[color]:<15}\t'
-                        f'{player.bonus_owned[color]}')
-            print(f"yellow\t{player.token_reserved['yellow']}")
-            if player.cards_reserved:
-                print('\nReserved cards:')
-                for card_id in player.cards_reserved:
-                    card = player.cards_reserved[card_id]
-                    print(f'{card_id:<6}\tLevel: {card.level}\t'
-                            f'\nPrestige points: {card.prestige_points}'
-                            f'\tBonus: {card.bonus_color}'
-                            '\nToken cost:')
-                    for color in card.token_cost:
-                        if card.token_cost[color] > 0:
-                            print(f'{color:<5}\t'
-                                    f'{card.token_cost[color]}')
+        self.show_game_nobles()
+        self.show_game_bank()
+        self.show_game_players()
+
         print("\nCards on the Table")
         # Print all cards on the Table
         for card_id in self.cards_on_table_level_1:
             print("Card_id\tLevel\tBonus\tPrestige Points")
             card = self.cards_on_table_level_1[card_id]
             print(f'{card_id:<5}\t{card.level:<5}\t{card.bonus_color:<5}\t'
-                    f'{card.prestige_points:<10}')
+                  f'{card.prestige_points:<10}')
             print("Token cost:")
             print("Green\tWhite\tBlue\tBlack\tRed")
             for color in card.token_cost:
@@ -109,7 +95,7 @@ class GameInterfaceConsole(GameInterface):
             print("Card_id\tLevel\tBonus\tPrestige Points")
             card = self.cards_on_table_level_2[card_id]
             print(f'{card_id:<5}\t{card.level:<5}\t{card.bonus_color:<5}\t'
-                    f'{card.prestige_points:<10}')
+                  f'{card.prestige_points:<10}')
             print("Token cost:")
             print("Green\tWhite\tBlue\tBlack\tRed")
             for color in card.token_cost:
@@ -119,21 +105,9 @@ class GameInterfaceConsole(GameInterface):
             print("Card_id\tLevel\tBonus\tPrestige Points")
             card = self.cards_on_table_level_3[card_id]
             print(f'{card_id:<5}\t{card.level:<5}\t{card.bonus_color:<5}\t'
-                    f'{card.prestige_points:<10}')
+                  f'{card.prestige_points:<10}')
             print("Token cost:")
             print("Green\tWhite\tBlue\tBlack\tRed")
             for color in card.token_cost:
                 print(f'{card.token_cost[color]:<5}', end='\t')
-            print('\n')
-        # TODO Print Nobles
-        print('\nNobles:')
-        for noble_id in self.nobles:
-            noble = self.nobles[noble_id]
-            print('Noble_id\tPrestige Points')
-            print(f'{noble_id:<5}\t{noble.prestige_points}')
-            print("Bonuses required:")
-            for color in noble.bonus_required:
-                if noble.bonus_required[color] > 0:
-                    print(f'{color:<5}\t'
-                            f'{noble.bonus_required[color]}')
             print('\n')
