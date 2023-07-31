dd1651d3f6851d7d092cc81a3f0d6ac05fdcde0f
Nikola Spasovski <nikola221295@hotmail.com>
2023-07-31 13:05:32 +0200

    Cleaned up the code in the main script

--- splendor_game.py
+++ splendor_game.py
@@ -1,14 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May  2 11:45:24 2022.
-
-@author: Nikola
-"""
-
 from game_base.game_interface import GameInterfaceConsole
-from random import seed
 
-seed(10)
+
 def main():
     console_interface = GameInterfaceConsole()
     console_interface.run()
