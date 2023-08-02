592e5f633602d9cbf5325fd915050d1705aeefdb
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-02 09:32:08 +0200

    Added basic add & remove tests

--- tests/game_base/test_tokens.py
+++ tests/game_base/test_tokens.py
@@ -1,6 +1,7 @@
 import pytest
 from game_base.tokens import Token, TokenBag
 
+
 class TestingTokenBagInit:
     def test_token_bag_initialization_default(self) -> None:
         token_bag = TokenBag()
@@ -12,7 +13,6 @@ class TestingTokenBagInit:
                     Token.YELLOW: 0}
         assert token_bag.tokens == expected
 
-
     def test_token_bag_initialization_just_standard_amount(self) -> None:
         token_bag = TokenBag(standard_amount=5)
         expected = {Token.GREEN: 5,
@@ -22,7 +22,6 @@ class TestingTokenBagInit:
                     Token.RED: 5,
                     Token.YELLOW: 5}
         assert token_bag.tokens == expected
-
 
     def test_token_bag_initialization_just_wildcard_amount(self) -> None:
         token_bag = TokenBag(wildcard_amount=5)
@@ -34,7 +33,6 @@ class TestingTokenBagInit:
                     Token.YELLOW: 5}
         assert token_bag.tokens == expected
 
-
     def test_token_bag_initialization_different_values(self) -> None:
         token_bag = TokenBag(standard_amount=5, wildcard_amount=3)
         expected = {Token.GREEN: 5,
@@ -44,7 +42,6 @@ class TestingTokenBagInit:
                     Token.RED: 5,
                     Token.YELLOW: 3}
         assert token_bag.tokens == expected
-
 
     def test_token_bag_initialization_same_values(self) -> None:
         token_bag = TokenBag(standard_amount=5, wildcard_amount=5)
@@ -56,20 +53,62 @@ class TestingTokenBagInit:
                     Token.YELLOW: 5}
         assert token_bag.tokens == expected
 
-
     def test_token_bag_initialization_negative_value_1(self) -> None:
         with pytest.raises(ValueError) as e:
             token_bag = TokenBag(standard_amount=-5,
-                                wildcard_amount=5)
-
+                                 wildcard_amount=5)
 
     def test_token_bag_initialization_negative_value_2(self) -> None:
         with pytest.raises(ValueError) as e:
             token_bag = TokenBag(standard_amount=5,
-                                wildcard_amount=-5)
-
+                                 wildcard_amount=-5)
 
     def test_token_bag_initialization_negative_value_3(self) -> None:
         with pytest.raises(ValueError) as e:
             token_bag = TokenBag(standard_amount=-5,
                                  wildcard_amount=-5)
+
+
+class TestingTokenBagAdd:
+    def test_token_bag_default_add_single(self) -> None:
+        token_bag = TokenBag(standard_amount=0)
+        amounts = {Token.GREEN: 2}
+        token_bag.add(amounts)
+        assert token_bag.tokens[Token.GREEN] == 2
+
+    def test_token_bag_default_add_multiple(self) -> None:
+        token_bag = TokenBag(standard_amount=0)
+        amounts = {Token.GREEN: 2, Token.RED: 3}
+        token_bag.add(amounts)
+        assert token_bag.tokens[Token.GREEN] == 2
+        assert token_bag.tokens[Token.RED] == 3
+
+    def test_token_bag_default_add_all(self) -> None:
+        token_bag = TokenBag(standard_amount=0)
+        amounts = {Token.GREEN: 1,
+                         Token.WHITE: 2,
+                         Token.BLUE: 3,
+                         Token.BLACK: 1,
+                         Token.RED: 2,
+                         Token.YELLOW: 3}
+        token_bag.add(amounts)
+        assert token_bag.tokens[Token.GREEN] == 1
+        assert token_bag.tokens[Token.WHITE] == 2
+        assert token_bag.tokens[Token.BLUE] == 3
+        assert token_bag.tokens[Token.BLACK] == 1
+        assert token_bag.tokens[Token.RED] == 2
+        assert token_bag.tokens[Token.YELLOW] == 3
+
+
+class TestingTokenBagRemove:
+    def test_token_bag_default_remove_single(self) -> None:
+        token_bag = TokenBag()
+        amounts = {Token.GREEN: 1}
+        with pytest.raises(ValueError) as e:
+            token_bag.remove(amounts)
+
+    def test_token_bag_default_remove_multiple(self) -> None:
+        token_bag = TokenBag()
+        amounts = {Token.GREEN: 2, Token.RED: 3}
+        with pytest.raises(ValueError) as e:
+            token_bag.remove(amounts)
