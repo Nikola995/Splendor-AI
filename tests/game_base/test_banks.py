5314268c7c81f6814c60530fe1a555d5eb1cfd57
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-02 12:42:41 +0200

    Added bank.add_token & bank.__str__ tests

--- tests/game_base/test_banks.py
+++ tests/game_base/test_banks.py
@@ -154,24 +154,84 @@ class TestingBankRemoveWildcard:
 
 class TestingBankAddToken:
     def test_bank_add_token_single(self) -> None:
-        assert False
+        bank = Bank()
+        amounts = {Token.GREEN: 2}
+        bank.add_token(amounts)
+        expected = {Token.GREEN: 9,
+                    Token.WHITE: 7,
+                    Token.BLUE: 7,
+                    Token.BLACK: 7,
+                    Token.RED: 7,
+                    Token.YELLOW: 5}
+        assert bank.token_available.tokens == expected
 
     def test_bank_add_token_multiple(self) -> None:
-        assert False
+        bank = Bank()
+        amounts = {Token.GREEN: 1,
+                   Token.WHITE: 2,
+                   Token.BLUE: 3}
+        bank.add_token(amounts)
+        expected = {Token.GREEN: 8,
+                    Token.WHITE: 9,
+                    Token.BLUE: 10,
+                    Token.BLACK: 7,
+                    Token.RED: 7,
+                    Token.YELLOW: 5}
+        assert bank.token_available.tokens == expected
 
     def test_bank_add_token_all(self) -> None:
-        assert False
+        bank = Bank()
+        amounts = {Token.GREEN: 1,
+                   Token.WHITE: 2,
+                   Token.BLUE: 3,
+                   Token.BLACK: 1,
+                   Token.RED: 2,
+                   Token.YELLOW: 3}
+        bank.add_token(amounts)
+        expected = {Token.GREEN: 8,
+                    Token.WHITE: 9,
+                    Token.BLUE: 10,
+                    Token.BLACK: 8,
+                    Token.RED: 9,
+                    Token.YELLOW: 8}
+        assert bank.token_available.tokens == expected
 
 
 class TestingBankStr:
     def test_bank_str_default(self) -> None:
-        assert False
+        bank = Bank()
+        assert str(bank) == ("Available Bank tokens:\n"
+                             "GREEN: 7\n"
+                             "WHITE: 7\n"
+                             "BLUE : 7\n"
+                             "BLACK: 7\n"
+                             "RED  : 7\n"
+                             "YELLOW: 5")
 
     def test_bank_str_empty(self) -> None:
-        assert False
+        bank = Bank()
+        for color in bank.token_available.tokens:
+            bank.token_available.tokens[color] = 0
+        assert str(bank) == ("Available Bank tokens:\n")
 
     def test_bank_str_not_all(self) -> None:
-        assert False
+        bank = Bank()
+        bank.token_available.tokens[Token.GREEN] = 0
+        assert str(bank) == ("Available Bank tokens:\n"
+                             "WHITE: 7\n"
+                             "BLUE : 7\n"
+                             "BLACK: 7\n"
+                             "RED  : 7\n"
+                             "YELLOW: 5")
 
-    def test_bank_str_remove(self) -> None:
-        assert False
+    def test_bank_str_post_remove(self) -> None:
+        bank = Bank()
+        tokens_to_remove = (Token.GREEN, Token.WHITE, Token.BLUE)
+        bank.remove_3_unique_color_tokens(tokens_to_remove)
+        assert str(bank) == ("Available Bank tokens:\n"
+                             "GREEN: 6\n"
+                             "WHITE: 6\n"
+                             "BLUE : 6\n"
+                             "BLACK: 7\n"
+                             "RED  : 7\n"
+                             "YELLOW: 5")
