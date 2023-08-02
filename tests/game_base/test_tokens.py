c49ea557b27e41faf22b6cb627ac72cc0509f550
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-02 10:51:39 +0200

    Added add & remove together tests

--- tests/game_base/test_tokens.py
+++ tests/game_base/test_tokens.py
@@ -287,3 +287,92 @@ class TestingTokenBagRemove:
                     Token.RED: 5,
                     Token.YELLOW: 5}
         assert token_bag.tokens == expected
+
+
+class TestingTokenBagAddAndRemove:
+    def test_token_bag_add_and_remove_same_amount(self) -> None:
+        token_bag = TokenBag()
+        amounts = {Token.GREEN: 1}
+        token_bag.add(amounts)
+        token_bag.remove(amounts)
+        expected = {Token.GREEN: 0,
+                    Token.WHITE: 0,
+                    Token.BLUE: 0,
+                    Token.BLACK: 0,
+                    Token.RED: 0,
+                    Token.YELLOW: 0}
+        assert token_bag.tokens == expected
+
+    def test_token_bag_add_and_remove_same_color(self) -> None:
+        token_bag = TokenBag()
+        amounts_to_add = {Token.GREEN: 3}
+        amounts_to_remove = {Token.GREEN: 1}
+        token_bag.add(amounts_to_add)
+        token_bag.remove(amounts_to_remove)
+        expected = {Token.GREEN: 2,
+                    Token.WHITE: 0,
+                    Token.BLUE: 0,
+                    Token.BLACK: 0,
+                    Token.RED: 0,
+                    Token.YELLOW: 0}
+        assert token_bag.tokens == expected
+
+    def test_token_bag_add_and_remove_different_color_single(self) -> None:
+        token_bag = TokenBag(standard_amount=7, wildcard_amount=5)
+        amounts_to_add = {Token.GREEN: 3}
+        amounts_to_remove = {Token.RED: 1}
+        token_bag.add(amounts_to_add)
+        token_bag.remove(amounts_to_remove)
+        expected = {Token.GREEN: 10,
+                    Token.WHITE: 7,
+                    Token.BLUE: 7,
+                    Token.BLACK: 7,
+                    Token.RED: 6,
+                    Token.YELLOW: 5}
+        assert token_bag.tokens == expected
+
+    def test_token_bag_add_and_remove_different_color_multiple(self) -> None:
+        token_bag = TokenBag(standard_amount=7, wildcard_amount=5)
+        amounts_to_add = {Token.GREEN: 2,
+                          Token.WHITE: 3,
+                          Token.YELLOW: 1}
+        amounts_to_remove = {Token.WHITE: 2,
+                             Token.BLUE: 2,
+                             Token.BLACK: 2}
+        token_bag.add(amounts_to_add)
+        token_bag.remove(amounts_to_remove)
+        expected = {Token.GREEN: 9,
+                    Token.WHITE: 8,
+                    Token.BLUE: 5,
+                    Token.BLACK: 5,
+                    Token.RED: 7,
+                    Token.YELLOW: 6}
+        assert token_bag.tokens == expected
+
+    def test_token_bag_add_and_remove_from_other_token_bag(self) -> None:
+        token_bag = TokenBag()
+        token_bag_to_add = TokenBag(standard_amount=5, wildcard_amount=4)
+        token_bag_to_remove = TokenBag(standard_amount=3)
+        token_bag.add(token_bag_to_add.tokens)
+        token_bag.remove(token_bag_to_remove.tokens)
+        expected = {Token.GREEN: 2,
+                    Token.WHITE: 2,
+                    Token.BLUE: 2,
+                    Token.BLACK: 2,
+                    Token.RED: 2,
+                    Token.YELLOW: 1}
+        assert token_bag.tokens == expected
+
+
+class TestingTokenBagStr:
+    def test_token_bag_str_default(self) -> None:
+        pass
+
+    def test_token_bag_str_single(self) -> None:
+        pass
+
+    def test_token_bag_str_multiple(self) -> None:
+        pass
+
+    def test_token_bag_str_all(self) -> None:
+        pass
