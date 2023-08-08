20bce84f1da2fcbe3c33681262bd6a80f77467d9
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-08 11:14:42 +0200

    Added some initial token action tests

--- tests/game_base/test_actions.py
+++ tests/game_base/test_actions.py
@@ -10,13 +10,80 @@ from tests.game_base.test_cards import TestingCardManager
 
 
 class TestingReserve2SameColorTokens:
+    def test_reserve_2_same_tokens_init_error_wildcard(self) -> None:
+        color = Token.YELLOW
+        with pytest.raises(ValueError) as e:
+            action = Reserve2SameColorTokens(color)
+
+    def test_reserve_2_same_tokens_can_perform_True(self) -> None:
+        raise NotImplementedError()
+
+    def test_reserve_2_same_tokens_can_perform_False_bank(self) -> None:
+        raise NotImplementedError()
+
+    def test_reserve_2_same_tokens_can_perform_False_player(self) -> None:
+        raise NotImplementedError()
+
     def test_reserve_2_same_tokens(self) -> None:
-        raise NotImplementedError
+        player_1 = Player('player_1')
+        player_2 = Player('player_2')
+        bank = Bank(num_players=2)
+        color_1 = Token.GREEN
+        action_1 = Reserve2SameColorTokens(color_1)
+        action_1.perform(player_1, bank)
+        assert player_1.token_reserved == TokenBag().add({color_1: 2})
+        assert bank.token_available == TokenBag(4, 5).remove({color_1: 2})
+        color_2 = Token.RED
+        action_2 = Reserve2SameColorTokens(color_2)
+        action_2.perform(player_2, bank)
+        assert player_2.token_reserved == TokenBag().add({color_2: 2})
+        assert bank.token_available == TokenBag(4, 5).remove({color_1: 2,
+                                                              color_2: 2})
 
 
 class TestingReserve3UniqueColorTokens:
+    def test_reserve_3_unique_tokens_init_error_ne_3(self) -> None:
+        colors = (Token.GREEN, Token.WHITE)
+        with pytest.raises(ValueError) as e:
+            action = Reserve3UniqueColorTokens(colors)
+
+    def test_reserve_3_unique_tokens_init_error_not_unique(self) -> None:
+        colors = (Token.GREEN, Token.WHITE, Token.WHITE)
+        with pytest.raises(ValueError) as e:
+            action = Reserve3UniqueColorTokens(colors)
+
+    def test_reserve_3_unique_tokens_init_error_wildcard(self) -> None:
+        colors = (Token.GREEN, Token.WHITE, Token.YELLOW)
+        with pytest.raises(ValueError) as e:
+            action = Reserve3UniqueColorTokens(colors)
+
+    def test_reserve_3_unique_tokens_can_perform_True(self) -> None:
+        raise NotImplementedError()
+
+    def test_reserve_3_unique_tokens_can_perform_False_bank(self) -> None:
+        raise NotImplementedError()
+
+    def test_reserve_3_unique_tokens_can_perform_False_player(self) -> None:
+        raise NotImplementedError()
+
     def test_reserve_3_unique_tokens(self) -> None:
-        raise NotImplementedError
+        player_1 = Player('player_1')
+        player_2 = Player('player_2')
+        bank = Bank(num_players=2)
+        colors_1 = (Token.GREEN, Token.WHITE, Token.RED)
+        action_1 = Reserve3UniqueColorTokens(colors_1)
+        action_1.perform(player_1, bank)
+        token_amounts_1 = dict.fromkeys(colors_1, 1)
+        assert player_1.token_reserved == TokenBag().add(token_amounts_1)
+        assert bank.token_available == TokenBag(4, 5).remove(token_amounts_1)
+        colors_2 = (Token.BLACK, Token.BLUE, Token.RED)
+        action_2 = Reserve3UniqueColorTokens(colors_2)
+        action_2.perform(player_2, bank)
+        token_amounts_2 = dict.fromkeys(colors_2, 1)
+        assert player_2.token_reserved == TokenBag().add(token_amounts_2)
+        assert bank.token_available == (TokenBag(4, 5)
+                                        .remove(token_amounts_1)
+                                        .remove(token_amounts_2))
 
 
 class TestingReserveCard:
