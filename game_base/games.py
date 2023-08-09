3a0e2b2e805127c669086c0289c8f5b919d284a0
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-09 11:31:31 +0200

    Changed error raise to condition check

--- game_base/games.py
+++ game_base/games.py
@@ -159,9 +159,8 @@ class Game:
         """Checks if the given action can be performed for the current
         player's move.
         """
-        if self.meta_data.state != GameState.IN_PROGRESS:
-            raise ValueError("Game isn't currently in progress")
-        return action.can_perform(self.get_current_player(), self.bank)
+        return (self.meta_data.state != GameState.IN_PROGRESS and
+                action.can_perform(self.get_current_player(), self.bank))
 
     def make_move_for_current_player(self, action: Action) -> None:
         """Performs the given action as the player's move and iterate the
