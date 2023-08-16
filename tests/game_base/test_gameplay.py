55fd2755cd14ca14177acaeffc304e968da2aeae
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-16 09:38:14 +0200

    Added test game playing with 2 players

--- tests/game_base/test_gameplay.py
+++ tests/game_base/test_gameplay.py
@@ -1,133 +1,69 @@
-# # %% Tests
-# # %%%
-# # game.current_player_to_move(verbose)
-# game.perform_action_for_current_player(
-#     Reserve3DifferentColorTokens(params={'color_list': ['green',
-#                                                         'white',
-#                                                         'black']}),
-#     verbose=verbose)
+import pytest
+from game_base.cards import CardGenerator
+from game_base.tokens import TokenBag, Token
+from game_base.players import Player
+from game_base.banks import Bank
+from game_base.actions import (ReserveCard, PurchaseCard,
+                               Reserve2SameColorTokens,
+                               Reserve3UniqueColorTokens)
+from game_base.games import Game, GameState
 
-# # %%%
-# # game.current_player_to_move(verbose)
-# game.perform_action_for_current_player(
-#     Reserve2SameColorTokens(params={'color': 'red'}),
-#     verbose=verbose)
-# # %%%
-# # game.current_player_to_move(verbose)
-# game.perform_action_for_current_player(
-#     ReserveCard(params={'card_id': 'card_39'}),
-#     verbose=verbose)
-# # %%%
-# winner = end_game_turn(game, 1)
-# print(f"The winner is {winner}")
 
-# # %%%
-# # game.current_player_to_move(verbose)
-# game.perform_action_for_current_player(
-#     Reserve3DifferentColorTokens(params={'color_list': ['green',
-#                                                         'white',
-#                                                         'black']}),
-#     verbose=verbose)
-# # %%%
-# # game.current_player_to_move(verbose)
-# game.perform_action_for_current_player(
-#     Reserve3DifferentColorTokens(params={'color_list': ['green',
-#                                                         'white',
-#                                                         'black']}),
-#     verbose=verbose)
-# # %%%
-# # game.current_player_to_move(verbose)
-
-# # =============================================================================
-# # print(game.current_player_perform_card_action(partial(purchase_a_card,
-# #                                     yellow_replaces = {"green":1,
-# #                                        "white":0,
-# #                                        "blue":0,
-# #                                        "black":0,
-# #                                        "red":0}),
-# #                             card_id = 'card_39', is_reserved = True,
-# #                             verbose = 1))
-# # =============================================================================
-
-# game.perform_action_for_current_player(
-#     PurchaseCard(params={'card_id': 'card_38',
-#                          'is_reserved': False,
-#                          'yellow_replaces': {"green": 1, "white": 0,
-#                                              "blue": 0, "black": 0,
-#                                              "red": 0}}),
-#     verbose=verbose)
-# # %%%
-# winner = end_game_turn(game, 1)
-# print(f"The winner is {winner}")
-# # %%%
-# # print(game.current_player_to_move())
-# # %%%
-# # print(game.curr_player_index)
-# # %%%
-# # print(game.cards_on_table_level_1)
-# # %%%
-# game.players[2].token_reserved['green'] = 3
-# game.players[2].token_reserved['red'] = 3
-# game.players[2].token_reserved['blue'] = 3
-# game.players[2].token_reserved['black'] = 3
-# game.players[2].token_reserved['white'] = 3
-# # %%%
-# # Should return an IncorrectInputError
-# # =============================================================================
-# # game.perform_action_for_current_player(
-# #     PurchaseCard(params={'card_id': 'card_38',
-# #                          'is_reserved': False,
-# #                          'yellow_replaces': {"green": 1, "white": 0,
-# #                                              "blue": 0, "black": 0,
-# #                                              "red": 0}}),
-# #     verbose=verbose)
-# # =============================================================================
-# game.perform_action_for_current_player(
-#     PurchaseCard(params={'card_id': 'card_38',
-#                          'is_reserved': False,
-#                          'yellow_replaces': {"green": 0, "white": 0,
-#                                              "blue": 0, "black": 0,
-#                                              "red": 0}}),
-#     verbose=verbose)
-
-# # %%%
-# winner = end_game_turn(game, 1)
-# print(f"The winner is {winner}")
-# # game.state(verbose=1)
-# # %%%
-# all_actions = game.all_possible_actions()
-# # print(all_actions)
-# for action_type in all_actions:
-#     print(f"Number of {action_type}: {len(all_actions[action_type])}")
-# # %%%
-# print(sum(game.current_player_to_move().token_reserved.values()))
-# # %%%
-# for a in all_actions['reserve_card']:
-#     print(a.print_string())
-# # %%%
-# for a in all_actions['purchase_card']:
-#     if a.params['is_reserved']:
-#         print(a.print_string())
-# # %%%
-# legal_actions = game.all_legal_actions()
-# for action_type in legal_actions:
-#     print(f"Number of legal {action_type}: {len(legal_actions[action_type])}")
-# # %%%
-# temp_tokens = game.bank.token_available
-# game.bank.token_available = {"green": 4, "white": 1,
-#                              "blue": 1, "black": 4,
-#                              "red": 0}
-
-# legal_actions = game.all_legal_actions()
-# for action_type in legal_actions:
-#     print(f"Number of legal {action_type}: {len(legal_actions[action_type])}")
-# game.bank.token_available = temp_tokens
-# # %%%
-# temp_tokens = game.current_player_to_move().token_reserved['black']
-# game.current_player_to_move().token_reserved['black'] = 4
-
-# legal_actions = game.all_legal_actions()
-# for action_type in legal_actions:
-#     print(f"Number of legal {action_type}: {len(legal_actions[action_type])}")
-# game.current_player_to_move().token_reserved['black'] = temp_tokens
-# # %%%
+def test_gameplay_game_1_players_2() -> None:
+    num_players = 2
+    players = [Player(f'test_player_{i + 1}') for i in range(num_players)]
+    game = Game(players=players,
+                cards=CardGenerator.generate_cards(shuffled=False))
+    game.initialize()
+    # Player 1
+    game.make_move_for_current_player(Reserve2SameColorTokens(Token.WHITE))
+    # Player 2
+    game.make_move_for_current_player(Reserve3UniqueColorTokens((Token.GREEN,
+                                                                 Token.BLUE,
+                                                                 Token.RED)))
+    assert game.meta_data.turns_played == 1
+    # Player 1
+    game.make_move_for_current_player(Reserve3UniqueColorTokens((Token.WHITE,
+                                                                 Token.BLACK,
+                                                                 Token.GREEN)))
+    # Player 2
+    game.make_move_for_current_player(Reserve3UniqueColorTokens((Token.WHITE,
+                                                                 Token.BLUE,
+                                                                 Token.RED)))
+    assert game.meta_data.turns_played == 2
+    # Player 1
+    card_reserved_1 = game.cards.get_all_cards_on_tables()[4]
+    game.make_move_for_current_player(ReserveCard(card_reserved_1))
+    # Player 2
+    card_2 = game.cards.get_all_cards_on_tables()[3]
+    game.make_move_for_current_player(ReserveCard(card_2))
+    assert game.meta_data.turns_played == 3
+    # Player 1
+    card_owned_1 = game.cards.get_all_cards_on_tables()[0]
+    game.make_move_for_current_player(PurchaseCard(card_owned_1))
+    # Player 2
+    game.make_move_for_current_player(PurchaseCard(card_2))
+    assert game.meta_data.turns_played == 4
+    assert card_reserved_1 in game.players[0].cards_reserved
+    assert game.players[0].num_reserved_cards == 1
+    assert card_owned_1 in game.players[0].cards_owned
+    assert game.players[0].bonus_owned.tokens[Token.RED] == 1
+    assert game.players[0].prestige_points == 1
+    expected_tokens_0 = {Token.BLACK: 1, Token.GREEN: 1}
+    assert game.players[0].token_reserved == TokenBag().add(expected_tokens_0)
+    assert card_2 not in game.players[1].cards_reserved
+    assert game.players[1].num_reserved_cards == 0
+    assert card_2 in game.players[1].cards_owned
+    assert game.players[1].bonus_owned.tokens[Token.RED] == 1
+    assert game.players[1].prestige_points == 0
+    expected_tokens_1 = {Token.RED: 2,
+                         Token.WHITE: 1,
+                         Token.YELLOW: 1}
+    assert game.players[1].token_reserved == TokenBag().add(expected_tokens_1)
+    expected_bank = Bank(num_players)
+    for color in expected_tokens_0:
+        expected_bank.remove_token({color: expected_tokens_0[color]})
+    for color in expected_tokens_1:
+        expected_bank.remove_token({color: expected_tokens_1[color]})
+    assert game.bank == expected_bank
+    assert game.meta_data.state == GameState.IN_PROGRESS
