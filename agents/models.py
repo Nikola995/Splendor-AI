a3da2373ad0b788d3f69fb3d929b08ce055a6a22
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-20 12:07:09 +0200

    Added correct default output_size for RandomModel

--- agents/models.py
+++ agents/models.py
@@ -4,7 +4,7 @@ from random import random
 
 @dataclass
 class RandomModel:
-    output_size: int = 20
+    output_size: int = 42
 
     def evaluate(self, game_state: list[int] = []) -> list[float]:
         return [random() for _ in range(self.output_size)]
