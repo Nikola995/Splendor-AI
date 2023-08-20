aee8e583eeb6387446f8d20822fb1c8033067de4
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-20 12:47:25 +0200

    Made simulation of n games

--- splendor_random_agent_sim.py
+++ splendor_random_agent_sim.py
@@ -3,26 +3,37 @@ from agents.agent import Agent
 from agents.models import RandomModel
 from game_base.players import Player
 
-def simulate_game(num_players: int = 4) -> int:
-    """Simulates an entire game with Random Choice Agents."""
-    players = [Player(f'random_{i + 1}') for i in range(num_players)]
+
+def simulate_game(agent: Agent, num_players_in_game: int = 4) -> bool:
+    """Simulates an entire game of given number of players with given agent."""
+    players = [Player(f'random_{i + 1}') for i in range(num_players_in_game)]
     game = Game(players=players)
     game.initialize()
     while game.meta_data.state != GameState.FINISHED:
-        agent = Agent(RandomModel())
         action, idx = agent.get_action(game)
         if action is None:
-            print("There was no valid action")
+            # print("There was no valid action")
             break
-        print(f"{agent} chose action {idx}: {action}")
+        # print(f"{agent} chose action {idx}: {action}")
         game.make_move_for_current_player(action)
-    if game.meta_data.state == GameState.FINISHED:
-        print(f"The winner is {game.get_winner()}")
-    
+    # if game.meta_data.state == GameState.FINISHED:
+    #     print(f"The winner is {game.get_winner()}")
+    return game.meta_data.state == GameState.FINISHED
+
 
 def main():
-    simulate_game(num_players=4)
-    
+    num_simulations = 100_000
+    agent = Agent(RandomModel())
+    num_stalemates = 0
+    for i in range(num_simulations):
+        ended_game = simulate_game(agent=agent,
+                                   num_players_in_game=4)
+        if not ended_game:
+            num_stalemates += 1
+        print('\r'+f"Game {i} finished", end='\r')
+    print(f"From {num_simulations} simulations using {agent}, "
+          f"{num_stalemates} games were stalemated, or "
+          f"{(num_stalemates * 100)/num_simulations: .2f}% of games")
 
 
 if __name__ == '__main__':
