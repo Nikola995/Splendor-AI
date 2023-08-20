ca8f1e19588e2aabfc9c747e500ad2617d85ee7b
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-20 11:46:40 +0200

    Added random choice model

---
+++ agents/models.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from random import random
+
+
+@dataclass
+class RandomModel:
+    output_size: int = 20
+
+    def evaluate(self, game_state: list[int] = []) -> list[float]:
+        return [random() for _ in range(self.output_size)]
+    
+    def __str__(self) -> str:
+        return "Random Choice"
