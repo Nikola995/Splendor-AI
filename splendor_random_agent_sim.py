7e835f9d1e3a7d46432740a0597e86dacaa1721a
Nikola Spasovski <nikola221295@hotmail.com>
2023-08-28 10:56:07 +0200

    Used tqdm for loop visualization

--- splendor_random_agent_sim.py
+++ splendor_random_agent_sim.py
@@ -20,23 +20,26 @@ def simulate_game(agent: Agent, num_players_in_game: int = 4) -> bool:
         game.make_move_for_current_player(action)
     # if game.meta_data.state == GameState.FINISHED:
     #     print(f"The winner is {game.get_winner()}")
-    # TODO: Return is_stalemate, num_turns, avg_action_idx, 
+    # TODO: Return is_stalemate, num_turns, avg_action_idx,
     return game.meta_data.state == GameState.FINISHED
 
 
 def main():
     num_simulations = 100_000
+    num_agents = 4
     agent = Agent(RandomModel())
     num_stalemates = 0
+    pbar = tqdm(desc='Simulated Games', unit=' games', initial=0,
+                total=num_simulations, leave=False)
     for i in range(num_simulations):
         ended_game = simulate_game(agent=agent,
-                                   num_players_in_game=4)
+                                   num_players_in_game=num_agents)
         if not ended_game:
             num_stalemates += 1
-        # TODO: Use tqdm library
-        print('\r'+f"Game {i} finished", end='\r')
+        pbar.update(1)
+    pbar.close()
     # TODO: Save data to data\simulations\random_choice.csv
-    print(f"From {num_simulations} simulations using {agent}, "
+    print(f"From {num_simulations} simulations using {num_agents} {agent}, "
           f"{num_stalemates} games were stalemated, or "
           f"{(num_stalemates * 100)/num_simulations: .2f}% of games")
 
